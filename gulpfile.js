e0e597b9007e782c10f2295efe60cae3bd44453c
starhao <starhao@danwi.me>
2017-05-12 15:51:07 +0800

    针对前端js,css文件进行压缩

--- gulpfile.js
+++ gulpfile.js
@@ -7,44 +7,59 @@ var babel = require('gulp-babel'),
     concat = require('gulp-concat'),         //文件合并
     uglify = require('gulp-uglify'),         //js压缩插件
     rename = require('gulp-rename');         //重命名
+var clean = require('gulp-clean');
+var eztask = require('gulp-eztasks');
+var gutil = require('gulp-util');
 
 var config = {};
 
 config.dist = 'dist';
 
-config.static = ['dist/js/app.js', 'resources/javascript/*.js'];
+config.static = ['app.js', 'routes/*.js',];
+
+config.cssFile = ['**/*.css', '!**/*.min.css', '!node_modules/**/*.css'];
+config.jsFile = ['resources/javascript/*.js', '!resources/javascript/*.min.js'];
+config.minFile = ['resources/javascript/*.min.js'];
 
 //压缩css
 gulp.task('minifycss', function () {
-  return gulp.src('resources/css/*.css')
+  return gulp.src(config.cssFile)
       .pipe(minifycss())
-      .pipe(gulp.dest('dist/css'))
+      .pipe(gulp.dest('dist'))
 });
 
 //es6Toes5
 gulp.task('useEs5', function () {
-  return gulp.src('app.js')
+  return gulp.src(config.jsFile)
       .pipe(babel({presets: ['es2015']}))
-      .pipe(gulp.dest('dist/js'))
-})
+      .pipe(gulp.dest('dist'))
+});
 
 //压缩js
-gulp.task('minify-js', ['useEs5'], function () {
-  return gulp.src('dist/js/*.js')
+gulp.task('minify', function () {
+  return gulp.src('./app.js')
       .pipe(uglify())
+      .on('error', function (err) {
+        gutil.log(gutil.colors.red(['Error']), err.toString());
+        this.emit('end')
+      })
       .pipe(rename({suffix: '.min'}))
-      .pipe(gulp.dest('dist/js'))
+      .pipe(gulp.dest('dist'))
+});
+
+//删除文件
+gulp.task('clean', function () {
+  return gulp.src('dist/*', {read: false})
+      .pipe(clean())
 });
 
 gulp.task('watch', function () {
   gulp.watch(['resources/css/*.css', 'resources/javascript/*.js'], ['minifycss', 'minify-js'])
 });
 
+gulp.task('ezminicss', eztask.minicss(config.cssFile));
+gulp.task('ezminijs', eztask.minijs(config.jsFile));
 
-// gulp.task('default', ['minifycss', 'minify-js', 'watch'], function () {
-//   console.log('gulp complete~')
-// });
-
-gulp.task('default',['minify-js'], function () {
+gulp.task('default', ['ezminicss', 'ezminijs'], function () {
   console.log('success');
-})
+});
